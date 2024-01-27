f424f40c00d9598dcccb1b30b63cd1528e3ebcdd
9vult <ninevult@gmail.com>
2024-01-26 21:28:49 -0500

    Rename Holo main file

--- Holo/HoloContext.cs
+++ Holo/HoloContext.cs
@@ -3,19 +3,19 @@ using System.IO;
 
 namespace Holo
 {
-    public class Holo
+    public class HoloContext
     {
-        private static readonly Lazy<Holo> _instance = new Lazy<Holo>(() => new Holo());
+        private static readonly Lazy<HoloContext> _instance = new Lazy<HoloContext>(() => new HoloContext());
         public static readonly string localappdata = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData, Environment.SpecialFolderOption.Create), "Ameko", "Holo");
         
-        public static Holo Instance => _instance.Value;
+        public static HoloContext Instance => _instance.Value;
         public static string HoloDirectory => localappdata;
         
         public PluginHandler PluginHandler { get; }
         public ConfigurationManager ConfigurationManager { get; }
         public Workspace Workspace { get; set; }
 
-        private Holo()
+        private HoloContext()
         {
             PluginHandler = new PluginHandler(Path.Combine(HoloDirectory, "plugins"));
             ConfigurationManager = new ConfigurationManager(HoloDirectory);
