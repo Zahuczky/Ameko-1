ca7f073b33f04b33cd23a3e63bad27f620487915
9vult <ninevult@gmail.com>
2024-01-27 12:03:46 -0500

    Read and write plaintext files

---
+++ AssCS/IO/TextParser.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AssCS.IO
+{
+    internal class TextParser : IFileParser
+    {
+        private char comment;
+        private char actor;
+        public File Load(string filepath)
+        {
+            File assFile = new File();
+            assFile.LoadDefault();
+
+            using var ioFile = System.IO.File.Open(filepath, System.IO.FileMode.Open);
+            var reader = new System.IO.StreamReader(ioFile);
+            while (reader.ReadLine() is { } line)
+            {
+                if (line.Equals(string.Empty)) continue;
+
+                var isComment = false;
+                var actorStr = string.Empty;
+
+                if (line.StartsWith(comment))
+                {
+                    line = line.Substring(1).Trim();
+                    isComment = true;
+                }
+                else
+                {
+                    var actorRegex = $@"^(.*){actor} (.+)";
+                    var match = Regex.Match(line, actorRegex);
+                    if (match.Success)
+                    {
+                        actorStr = match.Groups[1].Value;
+                        line = match.Groups[2].Value;
+                    }
+                }
+
+                var e = new Event(assFile.EventManager.NextId)
+                {
+                    Text = line,
+                    Comment = isComment,
+                    Actor = actorStr,
+                    Start = new Time(),
+                    End = new Time()
+                };
+                assFile.EventManager.AddLast(e);
+            }
+
+            reader.Close();
+            ioFile.Close();
+            return assFile;
+        }
+
+        public TextParser(char commentDelim = '#', char actorDelim = ':')
+        {
+            comment = commentDelim;
+            actor = actorDelim;
+        }
+    }
+}
---
+++ AssCS/IO/TextWriter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AssCS.IO
+{
+    internal class TextWriter : IFileWriter
+    {
+        private readonly File file;
+        private readonly string filepath;
+        private readonly Encoding encoding;
+        private readonly ConsumerInfo consumer;
+
+        public void Write(bool export)
+        {
+            using var ioFile = System.IO.File.Open(filepath, System.IO.FileMode.Open);
+            var writer = new System.IO.StreamWriter(ioFile, encoding);
+            writer.WriteLine($"# Exported by {consumer.Name} {consumer.Version} (AssCS {AssCSInfo.Version})");
+
+            foreach (var line in file.EventManager.Ordered)
+            {
+                if (line.Comment) continue;
+                var actor = line.Actor.Trim().Equals(string.Empty) ? "" : $"{line.Actor.Trim()}: ";
+                writer.WriteLine($"{actor}{line.GetStrippedText()}");
+
+                writer.Close();
+                ioFile.Close();
+            }
+
+        }
+
+        public TextWriter(File file, string filepath, ConsumerInfo consumer) : this(file, filepath, consumer, Encoding.UTF8) { }
+        public TextWriter(File file, string filepath, ConsumerInfo consumer, Encoding encoding)
+        {
+            this.file = file;
+            this.filepath = filepath;
+            this.encoding = encoding;
+            this.consumer = consumer;
+        }
+    }
+}
