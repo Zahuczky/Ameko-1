98114fa9d0d67ee649f50058478bd39878d30a61
9vult <ninevult@gmail.com>
2024-01-29 23:48:30 -0500

    Move "version bug" generation to a service

---
+++ Ameko/Services/AmekoService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ameko.Services
+{
+    public class AmekoService
+    {
+        public static string VERSION_BUG
+        {
+            get
+            {
+                var version = ThisAssembly.Git.SemVer.Label;
+                if (!version.Equals(string.Empty)) version = $"{version} ";
+                var position = $"{ThisAssembly.Git.Branch}-{ThisAssembly.Git.Commit}";
+                return $"{version}@ {position}";
+            }
+        }
+    }
+}
--- Ameko/ViewModels/MainViewModel.cs
+++ Ameko/ViewModels/MainViewModel.cs
@@ -4,11 +4,10 @@ namespace Ameko.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    private readonly string label = ThisAssembly.Git.SemVer.Label.Equals(string.Empty) ? $"{ThisAssembly.Git.SemVer.Label} " : "";
     public string CompiledTitle { get; }
     public MainViewModel()
     {
-        CompiledTitle = $"Ameko {label}@ {ThisAssembly.Git.Branch}-{ThisAssembly.Git.Commit}";
+        CompiledTitle = $"Ameko {AmekoService.VERSION_BUG}";
         HoloService.HoloInstance.Workspace.AddFileToWorkspace("c:\\test.ass");
     }
 }
