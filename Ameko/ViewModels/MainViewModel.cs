2947ff05147123d15f6c818da13fc5068bf99916
9vult <ninevult@gmail.com>
2024-02-03 01:54:59 -0500

    Working OpenFileDialog

--- Ameko/ViewModels/MainViewModel.cs
+++ Ameko/ViewModels/MainViewModel.cs
@@ -1,6 +1,7 @@
-﻿using Ameko.DataModels;
+﻿using Ameko.Services;
 using ReactiveUI;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Reactive.Linq;
 using System.Windows.Input;
 
@@ -8,18 +9,40 @@ namespace Ameko.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
+    private int selectedTabIndex;
     public Interaction<AboutWindowViewModel, AboutWindowViewModel?> ShowAboutDialog { get; }
+    public Interaction<MainViewModel, string?> ShowOpenFileDialog { get; }
     public ICommand ShowAboutDialogCommand { get; }
+    public ICommand ShowOpenFileDialogCommand { get; }
 
     public ObservableCollection<TabItemViewModel> Tabs { get; set; }
+    public int SelectedTabIndex
+    {
+        get => selectedTabIndex;
+        set => this.RaiseAndSetIfChanged(ref selectedTabIndex, value);
+    }
 
     public MainViewModel()
     {
         ShowAboutDialog = new Interaction<AboutWindowViewModel, AboutWindowViewModel?>();
+        ShowOpenFileDialog = new Interaction<MainViewModel, string?>();
+        
         ShowAboutDialogCommand = ReactiveCommand.Create(async () =>
         {
             var about = new AboutWindowViewModel();
             await ShowAboutDialog.Handle(about);
+        });
+        ShowOpenFileDialogCommand = ReactiveCommand.Create(async () =>
+        {
+            string filepath = await ShowOpenFileDialog.Handle(this);
+            if (filepath == null) return;
+
+            int id = HoloService.HoloInstance.Workspace.AddFileToWorkspace(filepath);
+            // TODO: Real 
+            Tabs?.Add(new TabItemViewModel(Path.GetFileNameWithoutExtension(filepath), 
+                                            HoloService.HoloInstance.Workspace.GetFile(id).File.EventManager.Head.Text
+                                            ));
+            SelectedTabIndex = (Tabs?.Count ?? 1) - 1; // lol
         });
 
         Tabs = [new TabItemViewModel("File1", "Content1"), new TabItemViewModel("File2", "Content2")];
--- Ameko/Views/MainWindow.axaml.cs
+++ Ameko/Views/MainWindow.axaml.cs
@@ -1,7 +1,9 @@
 ﻿using Ameko.ViewModels;
 using Avalonia.Controls;
+using Avalonia.Platform.Storage;
 using Avalonia.ReactiveUI;
 using ReactiveUI;
+using System.Reactive.Disposables;
 using System.Threading.Tasks;
 
 namespace Ameko.Views;
@@ -15,9 +17,34 @@ public partial class MainWindow : ReactiveWindow<MainViewModel>
         await dialog.ShowDialog(this);
     }
 
+    private async Task DoShowOpenFileDialogAsync(InteractionContext<MainViewModel, string?> interaction)
+    {
+        var files = await this.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "Open Subtitle File",
+            AllowMultiple = false,
+            FileTypeFilter = new[] {
+                new FilePickerFileType("ASS Files") { Patterns = new[] { "*.ass" } }
+            }
+        });
+        if (files.Count > 0)
+        {
+            var filepath = files[0].Path.AbsolutePath.ToString();
+            interaction.SetOutput(filepath);
+            return;
+        }
+        interaction.SetOutput(null);
+    }
+
     public MainWindow()
     {
         InitializeComponent();
-        this.WhenActivated(action => action(ViewModel!.ShowAboutDialog.RegisterHandler(DoShowAboutDialogAsync)));
+        this.WhenActivated((CompositeDisposable disposables) =>
+        {
+            ViewModel!.ShowAboutDialog.RegisterHandler(DoShowAboutDialogAsync);
+            ViewModel!.ShowOpenFileDialog.RegisterHandler(DoShowOpenFileDialogAsync);
+
+            Disposable.Create(() => { }).DisposeWith(disposables);
+        });
     }
 }
