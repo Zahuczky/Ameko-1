b347cdead306f87450d6e9e5e23aff4bef92c571
9vult <ninevult@gmail.com>
2024-02-07 22:03:06 -0500

    Display style names

--- AssCS/StyleManager.cs
+++ AssCS/StyleManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 
@@ -12,18 +13,22 @@ namespace AssCS
     {
         private readonly Dictionary<int, Style> styles;
 
+        public ObservableCollection<string> StyleNames { get; private set; }
+
         private int _id = 0;
         public int NextId => _id++;
 
         public int Set(int id, Style s)
         {
             styles[id] = s;
+            StyleNames.Add(s.Name);
             return s.Id;
         }
 
         public int Set(Style s)
         {
             styles[s.Id] = s;
+            StyleNames.Add(s.Name);
             return s.Id;
         }
 
@@ -46,19 +51,30 @@ namespace AssCS
         {
             foreach (Style style in styles.Values)
             {
-                if (style.Name == name) return styles.Remove(style.Id);
+                if (style.Name == name)
+                {
+                    StyleNames.Remove(name);
+                    return styles.Remove(style.Id);
+                }
             }
             return false;
         }
 
         public bool Remove(int id)
         {
-            return styles.Remove(id);
+            if (styles.ContainsKey(id))
+            {
+                var style = styles[id];
+                StyleNames.Remove(style.Name);
+                return styles.Remove(id);
+            }
+            return false;
         }
 
         public void Clear()
         {
             styles.Clear();
+            StyleNames.Clear();
         }
 
         public List<Style> GetAll()
@@ -75,16 +91,19 @@ namespace AssCS
         public StyleManager(StyleManager source)
         {
             styles = new Dictionary<int, Style>(source.styles);
+            StyleNames = new ObservableCollection<string>(source.StyleNames);
         }
 
         public StyleManager(File source)
         {
             styles = new Dictionary<int, Style>(source.StyleManager.styles);
+            StyleNames = new ObservableCollection<string>(source.StyleManager.StyleNames);
         }
 
         public StyleManager()
         {
             styles = new Dictionary<int, Style>();
+            StyleNames = new ObservableCollection<string>();
         }
     }
 }
