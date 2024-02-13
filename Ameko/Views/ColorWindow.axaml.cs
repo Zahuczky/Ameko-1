92a46cefdc447b26d6487b95215f07235a336891
9vult <ninevult@gmail.com>
2024-02-13 16:51:13 -0500

    Link color window with style editor

--- Ameko/Views/ColorWindow.axaml.cs
+++ Ameko/Views/ColorWindow.axaml.cs
@@ -1,12 +1,27 @@
+using Ameko.ViewModels;
 using Avalonia.Controls;
+using Avalonia.ReactiveUI;
+using ReactiveUI;
+using System;
+using System.Reactive.Disposables;
 
 namespace Ameko.Views
 {
-    public partial class ColorWindow : Window
+    public partial class ColorWindow : ReactiveWindow<ColorWindowViewModel>
     {
         public ColorWindow()
         {
             InitializeComponent();
+
+            this.WhenActivated((CompositeDisposable disposables) =>
+            {
+                if (ViewModel != null)
+                {
+                    ViewModel.SelectColorCommand.Subscribe(Close);
+                }
+
+                Disposable.Create(() => { }).DisposeWith(disposables);
+            });
         }
     }
 }
--- Ameko/Views/StyleEditorWindow.axaml.cs
+++ Ameko/Views/StyleEditorWindow.axaml.cs
@@ -1,12 +1,35 @@
+using Ameko.ViewModels;
+using AssCS;
 using Avalonia.Controls;
+using Avalonia.ReactiveUI;
+using ReactiveUI;
+using System.Reactive.Disposables;
+using System.Threading.Tasks;
 
 namespace Ameko.Views
 {
-    public partial class StyleEditorWindow : Window
+    public partial class StyleEditorWindow : ReactiveWindow<StyleEditorViewModel>
     {
+        private async Task DoShowDialogAsync(InteractionContext<ColorWindowViewModel, Color?> interaction)
+        {
+            var dialog = new ColorWindow();
+            dialog.DataContext = interaction.Input;
+            var result = await dialog.ShowDialog<Color?>(this);
+            interaction.SetOutput(result);
+        }
+
         public StyleEditorWindow()
         {
             InitializeComponent();
+            this.WhenActivated((CompositeDisposable disposables) =>
+            {
+                if (ViewModel != null)
+                {
+                    ViewModel.ShowDialog.RegisterHandler(DoShowDialogAsync);
+                }
+
+                Disposable.Create(() => { }).DisposeWith(disposables);
+            });
         }
     }
 }
