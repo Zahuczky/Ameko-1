90c2a80e586db6f3db3408e878f69fde8aaedc68
9vult <ninevult@gmail.com>
2024-02-25 10:34:10 -0500

    Add script exported methods

--- Holo/HoloScript.cs
+++ Holo/HoloScript.cs
@@ -33,25 +33,55 @@ namespace Holo
         /// Will be used for updates via Dependency Control
         /// </summary>
         public double Version { get; }
-
+        /// <summary>
+        /// Name of the default submenu to put the script under
+        /// </summary>
         public string? SubmenuName { get; }
+        /// <summary>
+        /// Array of qualified names being exported
+        /// </summary>
+        /// <remarks>
+        /// Qualified name should follow the format <c>[scriptqname].func</c>.
+        /// Handling of the qname must be provided in <c>Execute(qname)</c>.
+        /// </remarks>
+        public string[] ExportedMethods { get; }
+        
         /// <summary>
         /// Entry point
         /// </summary>
         /// <returns>Result of the script's execution</returns>
         public abstract Task<ExecutionResult> Execute();
+        /// <summary>
+        /// Entry point for exported functions
+        /// </summary>
+        /// <remarks>
+        /// You <b>MUST</b> override this method if your script uses exported functions!
+        /// The default implementation calls <c>Execute()</c> outright.
+        /// </remarks>
+        /// <param name="qname">Qualified name of the method</param>
+        /// <returns>Result of the script's execution</returns>
+        public virtual Task<ExecutionResult> Execute(string qname) { return Execute(); }
 
-        public HoloScript() : this("", "", "", "", -1.0, null)
+        public HoloScript() : this("", "", "", "", -1.0, new string[0], null)
         {
 
         }
-        protected HoloScript(string name, string qualifiedName, string description, string author, double version, string? submenuName = null)
+
+        protected HoloScript(
+            string name,
+            string qualifiedName,
+            string description,
+            string author,
+            double version,
+            string[] exportedMethods,
+            string? submenuName = null)
         {
             Name = name;
             QualifiedName = qualifiedName;
             Description = description;
             Author = author;
             Version = version;
+            ExportedMethods = exportedMethods;
             SubmenuName = submenuName;
         }
     }
